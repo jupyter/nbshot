9923dc1f5e5f4612950329d11013b28b40bb7044
Kyle Kelley <rgbkrk@gmail.com>
2014-11-26 07:04:14 +0000

    Zoom out.

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 var restify = require('restify');
 
 var webshot = require('webshot');
-var fs      = require('fs');
 
 function twitterCard(req, res, next) {
 	res.setHeader('content-type', 'image/png');
 	var options = {
-	  screenSize: {
+    screenSize: {
+      width: 280
+    , height: 150
+    }
+  , shotSize: {
 	    width: 280
 	  , height: 150
 	  }
-	, shotSize: {
-	    width: 280
-	  , height: 150
-	  }
+  , zoomFactor: 0.5
 	}
 	
 	webshot('nbviewer.ipython.org', options, function(err, renderStream) {
-    renderStream.pipe(res)
+    renderStream.pipe(res);
 	});
 }
 
 var server = restify.createServer();
-server.get('/t.png', twitterCard);
+server.get('/test.png', twitterCard);
 
 server.listen(8181, function() {
 	console.log('%s listening at %s', server.name, server.url);
