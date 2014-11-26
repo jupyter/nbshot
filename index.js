79c226c2f0c08a3a5c48c2ebb62b7e6c00ddb3c2
Kyle Kelley <rgbkrk@gmail.com>
2014-11-26 06:52:29 +0000

    Hokey restify API!

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+var restify = require('restify');
+
 var webshot = require('webshot');
 var fs      = require('fs');
 
-webshot('nbviewer.ipython.org', function(err, renderStream) {
-  var file = fs.createWriteStream('nbviewer.ipython.org.png', {encoding: 'binary'});
-  renderStream.on('data', function(data) {
-    file.write(data.toString('binary'), 'binary');
-  });
-});
+function twitterCard(req, res, next) {
+	res.setHeader('content-type', 'image/png');
+	var options = {
+	  screenSize: {
+	    width: 280
+	  , height: 150
+	  }
+	, shotSize: {
+	    width: 280
+	  , height: 150
+	  }
+	}
+	
+	webshot('nbviewer.ipython.org', options, function(err, renderStream) {
+    renderStream.pipe(res)
+	});
+}
 
+var server = restify.createServer();
+server.get('/t.png', twitterCard);
+
+server.listen(8181, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
