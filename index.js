d19c75bf77c77ed5c2719b7e36c83ee96f0541ff
Kyle Kelley <rgbkrk@gmail.com>
2014-11-26 07:05:01 +0000

    Whitespace fixes.

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 var restify = require('restify');
-
 var webshot = require('webshot');
 
 function twitterCard(req, res, next) {
-	res.setHeader('content-type', 'image/png');
-	var options = {
+  res.setHeader('content-type', 'image/png');
+  var options = {
     screenSize: {
       width: 280
     , height: 150
     }
   , shotSize: {
-	    width: 280
-	  , height: 150
-	  }
+      width: 280
+    , height: 150
+    }
   , zoomFactor: 0.5
-	}
-	
-	webshot('nbviewer.ipython.org', options, function(err, renderStream) {
+  }
+  
+  webshot('nbviewer.ipython.org', options, function(err, renderStream) {
     renderStream.pipe(res);
-	});
+  });
 }
 
 var server = restify.createServer();
 server.get('/test.png', twitterCard);
 
 server.listen(8181, function() {
-	console.log('%s listening at %s', server.name, server.url);
+  console.log('%s listening at %s', server.name, server.url);
 });
